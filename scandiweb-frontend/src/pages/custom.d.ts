283618d4f0107af12ca2f4747ce47cb9645da9b9
Correa <150452188+JamesdenCorrea@users.noreply.github.com>
2025-06-10 21:37:38 +0800

    Finalize frontend: modal fixes, attribute default, Apollo setup

--- scandiweb-frontend/src/pages/custom.d.ts
+++ scandiweb-frontend/src/pages/custom.d.ts
@@ -1,11 +1,10 @@
 declare module './Header.jsx' {
-  import React from 'react';
   const Header: React.FC<any>;
   export default Header;
 }
 
 declare module './ProductCard.jsx' {
-  import React from 'react';
+
   const ProductCard: React.FC<any>;
   export default ProductCard;
 }
