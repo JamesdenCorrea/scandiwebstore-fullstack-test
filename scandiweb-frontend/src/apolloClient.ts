658e2be8413df3ad088c200aa4fdfb53da8771e4
Correa <150452188+JamesdenCorrea@users.noreply.github.com>
2025-06-10 23:28:45 +0800

    Fix: Update build config for Vercel

--- scandiweb-frontend/src/apolloClient.ts
+++ scandiweb-frontend/src/apolloClient.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'https://appealing-nature-production.up.railway.app/graphql.php',
+  uri: 'http://localhost:8000/graphql.php',
   cache: new InMemoryCache(),
 });
 
